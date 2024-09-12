e1ac31d119aa4b97d8b8a90ed18802ff6b256375
Omar <galvezomar28@gmail.com>
2024-09-11 23:29:50 -0600

    Inicialización de la Práctica 1

---
+++ src/ipctec/IPCTEC.java
@@ -0,0 +1,17 @@
+
+package ipctec;
+
+/**
+ *
+ * @author Omar
+ */
+public class IPCTEC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
